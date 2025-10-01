0273279e856c8c7504ace2b3ff6e2a1b3f31ea66
2a33m <mdsaeem.mulanthala@gmail.com>
2025-10-01 14:52:36 +0530

    Delete project/backend/model/flipcart.js

--- project/backend/model/flipcart.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-var schema=mongoose.Schema({
-    name:String,
-    password:String,
-})
-var flipCart=mongoose.model("FlipCartSign",schema)
-module.exports=flipCart
